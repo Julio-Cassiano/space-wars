6938ec6e2c51297d39036dc3d99ecb3ed436e83a
Bruno Alencar <alencarb98@gmail.com>
2024-11-20 20:48:53 -0300

    feat: primeira versao Space Wars

--- src/classes/Invader.js
+++ src/classes/Invader.js
@@ -1,6 +1,12 @@
 import { PATH_INVADER_IMG } from "../utils/constants.js";
 import Projectile from "./Projectile.js";
 
+if(PATH_INVADER_IMG) {
+    console.log("existe a imagem")
+} else {
+    console.log("NAO EXISTE")
+}
+
 class Invader {
     constructor(position, velocity) {
         this.width = 50 * 0.8;
@@ -26,6 +32,14 @@ class Invader {
         this.position.x += this.velocity;
     }
 
+    moveDown(){
+        this.position.y += this.height;
+    }
+
+    incrementVelocity(boost){
+        this.velocity += boost
+    }
+
     draw(ctx) {
         ctx.drawImage(
             this.image,
@@ -39,11 +53,21 @@ class Invader {
     shoot(projectiles) {
         const p = new Projectile({
             x: this.position.x + this.width / 2 - 1,
-            y: this.position.y + 10,
+            y: this.position.y + this.height,
         }, -10);
 
         projectiles.push(p);
     }
+
+    hit(projectile){
+        return(
+            projectile.position.x >= this.position.x &&
+            projectile.position.x <= this.position.x + this.width &&
+            projectile.position.y >= this.position.y &&
+            projectile.position.y <= this.position.y + this.height
+        );
+    }
+
 }
 
 export default Invader;
--- src/classes/Player.js
+++ src/classes/Player.js
@@ -3,6 +3,8 @@ import Projectile from "./Projectile.js";
 
 class Player {
     constructor(canvasWidth, canvasHeight) {
+
+        this.alive = true;
         this.width = 48 * 2;
         this.height = 48 * 2;
         this.velocity = 6;
@@ -80,6 +82,16 @@ class Player {
 
         projectiles.push(p);
     }
+
+    hit(projectile){
+        return(
+            projectile.position.x >= this.position.x + 20 &&
+            projectile.position.x <= this.position.x + 20 + this.width - 38 &&
+            projectile.position.y >= this.position.y + 22 &&
+            projectile.position.y <= this.position.y + 22 + this.height - 34
+        );
+    }
+
 }
 
 export default Player;
